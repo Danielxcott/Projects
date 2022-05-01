26796c63292cdea459869c4f41f7c7924bbe786b
Thein Zan <95298352+Danielxcott@users.noreply.github.com>
2022-05-01 11:58:33 +0630

    Music Player

---
+++ Music player/data.js
@@ -0,0 +1,39 @@
+let songs=[
+    {
+        name: 'just like magic',
+        path: 'song/jlm.m4a',
+        artist: 'Ariana Grande',
+        cover: 'img/ariana.webp'
+    },
+    {
+        name: 'My hair',
+        path: 'song/mh.m4a',
+        artist: 'Ariana Grande',
+        cover: 'img/ariana.webp'
+    },
+    {
+        name: 'Worst Behaviors',
+        path: 'song/wbh.m4a',
+        artist: 'Ariana Grande',
+        cover: 'img/ariana.webp'
+    },
+    {
+        name: 'Off The Table',
+        path: 'song/ot.m4a',
+        artist: 'Ariana Grande',
+        cover: 'img/ariana.webp'
+    },
+    {
+        name: 'Block Me Out',
+        path: 'song/bmo.m4a',
+        artist: 'Gracia Abrams',
+        cover: 'img/gracia.jpeg'
+    },
+    {
+        name: 'As It Was',
+        path: 'song/aiw.m4a',
+        artist: 'Harry Style',
+        cover: 'img/harry.jpg.webp'
+    },
+
+]
